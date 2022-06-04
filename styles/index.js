da1ecb56a04fede3f5174a1be4c109fc2cdbcd1d
simretB05 <happysimret@gmai.com>
2022-06-03 18:49:16 -0600

    fix image issue on collection section

---
+++ styles/index.js
@@ -0,0 +1,6 @@
+const dropDown= document.querySelector(".sb1")
+const bars= document.querySelector(".fa-bars")
+bars.addEventListener("click",function(){
+    dropDown.classList.toggle("change")
+console.log(bars);
+}) 
